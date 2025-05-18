1401c475923d67df227a74b6247bc22b4bee28a9
Hao <wenhaoy.0428@gmail.com>
2025-05-19 00:53:40 +0800

    120
    Signed-off-by: Hao <wenhaoy.0428@gmail.com>

---
+++ 120/solution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include "../util/util.h"
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minimumTotal(vector<vector<int>> &triangle)
+    {
+    }
+};
+
+class SolutionTest : public testing::Test
+{
+protected:
+    Solution solution;
+};
+
+TEST_F(SolutionTest, test_001)
+{
+    string word1 = "";
+    string word2 = "";
+    bool result = solution.closeStrings(word1, word2);
+    ASSERT_TRUE(result);
+}
