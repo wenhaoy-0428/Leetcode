36135e4d8f6d3e0283c0a6a0b1ec4f4d0bf393f1
Hao <wenhaoy.0428@gmail.com>
2025-05-28 16:12:29 +0800

    1
    Signed-off-by: Hao <wenhaoy.0428@gmail.com>

--- 1/2025-05-28/solution.cpp
+++ 1/2025-05-28/solution.cpp
@@ -7,6 +7,32 @@
 
 using namespace std;
 
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> res;
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            mp[nums[i]] = i;
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (mp.find(target - nums[i]) != mp.end() && mp[target - nums[i]] != i)
+            {
+                res.push_back(i);
+                res.push_back(mp[target - nums[i]]);
+                return res;
+            }
+        }
+        return res;
+    }
+};
+
 class SolutionTest : public testing::Test
 {
 protected:
