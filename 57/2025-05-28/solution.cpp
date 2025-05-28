82dfb38185543a8959492b9090d4d6038a8dca5d
Hao <wenhaoy.0428@gmail.com>
2025-05-28 16:13:49 +0800

    57
    Signed-off-by: Hao <wenhaoy.0428@gmail.com>

---
+++ 57/2025-05-28/solution.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include "../util/util.h"
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
+    {
+        vector<vector<int>> res;
+
+        int i = 0, n = intervals.size();
+        while (i < n && intervals[i][1] < newInterval[0])
+        {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
+        while (i < n && intervals[i][0] <= newInterval[1])
+        {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        res.push_back(newInterval);
+        while (i < n)
+        {
+            res.push_back(intervals[i]);
+            i++;
+        }
+        return res;
+    }
+};
+
+class SolutionTest : public testing::Test
+{
+protected:
+    Solution solution;
+};
+
+TEST_F(SolutionTest, test_001)
+{
+    string word1 = "";
+    string word2 = "";
+    bool result = solution.closeStrings(word1, word2);
+    ASSERT_TRUE(result);
+}
