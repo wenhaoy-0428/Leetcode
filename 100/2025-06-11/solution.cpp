c972195326f42ee37907e7435f09f260447cf2d0
Hao <wenhaoy.0428@gmail.com>
2025-06-11 03:17:12 +0800

    100
    Signed-off-by: Hao <wenhaoy.0428@gmail.com>

---
+++ 100/2025-06-11/solution.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include "../util/util.h"
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if (p == NULL && q == NULL)
+            return true;
+        if (p == NULL || q == NULL || p->val != q->val)
+        {
+            return false;
+        }
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+class SolutionTest : public testing::Test
+{
+protected:
+    Solution solution;
+};
+
+TEST_F(SolutionTest, test_001)
+{
+    string word1 = "";
+    string word2 = "";
+    bool result = solution.closeStrings(word1, word2);
+    ASSERT_TRUE(result);
+}
